c037003d9b9d16bd5624abc63b50ce6c11c405c1
Jose Luis Aguilar <jlaguigom@gmail.com>
2019-10-05 19:27:59 +0200

    Updates

---
+++ precache-manifest.7a0a7f3a403b34cf8b7652a76bab53f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499aee5108495f6f6335f6791c9c11ac",
+    "url": "/react_course_masterfmws/index.html"
+  },
+  {
+    "revision": "b71567101f5f3f4b30a1",
+    "url": "/react_course_masterfmws/static/css/main.d1d5d802.chunk.css"
+  },
+  {
+    "revision": "b81b0ed23b94f827f3ab",
+    "url": "/react_course_masterfmws/static/js/2.19119440.chunk.js"
+  },
+  {
+    "revision": "b71567101f5f3f4b30a1",
+    "url": "/react_course_masterfmws/static/js/main.202f0440.chunk.js"
+  },
+  {
+    "revision": "45017214474823510b08",
+    "url": "/react_course_masterfmws/static/js/runtime-main.b7591c39.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react_course_masterfmws/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "1a36cc4d7f1b283edce389cb41bddb1f",
+    "url": "/react_course_masterfmws/static/media/react.1a36cc4d.svg"
+  }
+]);
