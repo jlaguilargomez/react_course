366088c3dc3efcf470486afcbc3416dd325a8939
Jose Luis Aguilar <jlaguigom@gmail.com>
2019-10-06 15:51:25 +0200

    añado reactividad al formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -12,9 +12,36 @@ class Formulario extends PureComponent {
     generoMujerRef = React.createRef();
     generoOtroRef = React.createRef();
 
+    state = {
+        user: {}
+    };
+
     recibirFormulario = (e) => {
         e.preventDefault();
-        console.log(this.nombreRef.current.value)
+
+        var genero = 'hombre';
+
+        if(this.generoHombreRef.current.checked){
+            genero = this.generoHombreRef.current.value;
+        }else if(this.generoMujerRef.current.checked){
+            genero = this.generoMujerRef.current.value;
+        }else{
+            genero = this.generoOtroRef.current.value;
+        }
+
+        var user = {
+            nombre: this.nombreRef.current.value,
+            apellidos: this.apellidosRef.current.value,
+            bio: this.bioRef.current.value,
+            genero: genero,
+            
+        }
+
+        this.setState({
+            user: user
+        })
+
+        console.log(user);
     }
 
     render() {
@@ -26,7 +53,18 @@ class Formulario extends PureComponent {
                 />
         
                 <div id="content">
-                    <form action="#" className="mid-form" onSubmit={this.recibirFormulario}>
+
+                    {
+                        this.state.user.nombre &&
+                        <div id="user-data">
+                            <p>Nombre: <strong>{this.state.user.nombre}</strong></p>
+                            <p>Apellidos: <strong>{this.state.user.apellidos}</strong></p>
+                            <p>Bio: <strong>{this.state.user.bio}</strong></p>
+                            <p>Género: <strong>{this.state.user.genero}</strong></p>
+                        </div>
+                    }
+
+                    <form action="#" className="mid-form" onChange={this.recibirFormulario}>
                         <div className="form-group">
                             <label for="nombre">Nombre</label>
                             <input type="text" name="nombre" id="nombre" ref={this.nombreRef}/>
@@ -34,7 +72,7 @@ class Formulario extends PureComponent {
 
                         <div className="form-group">
                             <label for="apellidos">Apellidos</label>
-                            <input type="text" name="apellidos" id="apellidos" ref={this.appelidosRef} />
+                            <input type="text" name="apellidos" id="apellidos" ref={this.apellidosRef} />
                         </div>
 
                         <div className="form-group">
